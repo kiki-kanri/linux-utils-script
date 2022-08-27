94fc1af02bc9f774cb30d557cb37263a1275d9e1
kiki-kanri <a470666@gmail.com>
2022-08-28 00:41:10 +0800

    update docker script

--- install_docker.sh
+++ install_docker.sh
@@ -4,7 +4,7 @@
 sh ./upgrade_package.sh
 ([ $os_id = "centos" ] && d="") || d=".io"
 sudo $pgm install -y docker$d
-grep docker /etc/group || sudo groupadd docker
+sudo groupadd docker
 sudo usermod -aG docker $USER
 sudo systemctl stop docker
 sudo systemctl start docker
