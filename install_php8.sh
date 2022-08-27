3effaec7c058bc2e1360cb4ef0640abd3fc4432b
kiki-kanri <a470666@gmail.com>
2022-08-27 23:46:38 +0800

    更新php script

--- install_php8.sh
+++ install_php8.sh
@@ -9,10 +9,12 @@ if [ $os_id = "centos"]; then
 	sudo yum-config-manager --disable 'remi-php*'
 	sudo yum-config-manager --enable remi-php$(printf $php_version | tr -d ".")
 	sudo $pgm install -y php php-perl php-{bcmath,cli,common,curl,fpm,gd,imap,mbstring,mysql,readline,redis,swoole,xml,zip}
+	php_v="php"
 else
 	sudo apt-get install -y lsb-release ca-certificates apt-transport-https software-properties-common
 	sudo add-apt-repository ppa:ondrej/php
 	sudo $pgm install -y php$php_version php$php_version-{cli,common,curl,fpm,gd,imap,mbstring,mysql,readline,redis,xml,zip}
+	php_v="php$php_version"
 fi
 
 [ $os_id = "centos" ] && sudo $pgm install -y php-json
