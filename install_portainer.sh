298d7b408bc63ec58180e80e3de23992003ceaa0
kiki-kanri <a470666@gmail.com>
2022-08-28 00:17:35 +0800

    update script

--- install_portainer.sh
+++ install_portainer.sh
@@ -1,7 +1,6 @@
 # Install portainer
 . ./get_os_version.sh
 
-docker ps || \
-echo "Can't use docker command! Check your docker state and permissions." && exit
+docker ps || (echo "Can't use docker command! Check your docker state and permissions." && exit)
 
 docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
