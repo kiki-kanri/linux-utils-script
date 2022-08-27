902ecbb1eb4f86f9b331f7fd723e394d98a59b95
kiki-kanri <a470666@gmail.com>
2022-08-28 00:59:10 +0800

    fixed mariadb not read os file

--- install_mariadb.sh
+++ install_mariadb.sh
@@ -1,4 +1,6 @@
 # Install mariadb 10.9
+. ./get_os_version.sh
+
 cd /tmp/
 curl -LsS -O https://downloads.mariadb.com/MariaDB/mariadb_repo_setup && \
 sudo bash mariadb_repo_setup --mariadb-server-version=10.9 && \
