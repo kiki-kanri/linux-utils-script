dd819335b099170f784190a05465dcb66cd68eef
kiki-kanri <a470666@gmail.com>
2022-08-28 10:57:14 +0800

    update nginx script

--- install_nginx.sh
+++ install_nginx.sh
@@ -2,10 +2,16 @@
 . ./get_os_version.sh
 
 if [ $os_id = "centos" ]; then
+	sudo $pgm install yum-utils
 	sudo cp ./centos/nginx.repo /etc/yum.repos.d/nginx.repo
 else
 	cd /tmp/
-	curl -O https://nginx.org/keys/nginx_signing.key && sudo apt-key add ./nginx_signing.key
+	sudo $pgm install gnupg2 ca-certificates lsb-release ubuntu-keyring
+	curl https://nginx.org/keys/nginx_signing.key | gpg --dearmor \
+	| sudo tee /usr/share/keyrings/nginx-archive-keyring.gpg > /dev/null
+	gpg --dry-run --quiet --import --import-options import-show /usr/share/keyrings/nginx-archive-keyring.gpg
+	echo "deb [signed-by=/usr/share/keyrings/nginx-archive-keyring.gpg] http://nginx.org/packages/ubuntu `lsb_release -cs` nginx" \
+    | sudo tee /etc/apt/sources.list.d/nginx.list
 	cd $(pwd)
 fi
 
