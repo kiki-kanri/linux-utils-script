be1989f065c1cd709eec7deaef58b9720bb645e6
kiki-kanri <a470666@gmail.com>
2022-08-29 02:10:07 +0800

    fix centos install_base bug

--- install_base_packages.sh
+++ install_base_packages.sh
@@ -2,5 +2,5 @@
 . ./get_os_version.sh
 
 sh ./upgrade_package.sh
-[ $os_id = "centos" ] && sudo $pgm install -y epel-release && sh ./update_package.sh
+[ $os_id = "centos" ] && sudo $pgm install -y epel-release && sh sh ./upgrade_package.sh
 sudo $pgm install -y curl gcc htop make net-tools perl screen tar tmux unzip vim wget
